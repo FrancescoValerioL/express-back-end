4c68f49709e4c01560c1961e7cfd78e8edd910fe
Francesco Valerio <70902884+FrancescoValerioL@users.noreply.github.com>
2022-05-12 11:10:20 +0200

    implementate prime funzioni di get, ricerca per genere, e lista completa, tutte divise per pagina

--- index.js
+++ index.js
@@ -1,10 +1,84 @@
 
+
+const { default: axios } = require('axios')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 2000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/api/movie' ,  (req , resp) =>{
+
+    
+  console.log("CALLING MOVIE ")
+  const country = req.query.country
+  const axios = require('axios').default;
+  // Make a request for a user with a given ID
+  axios.get("https://api.themoviedb.org/3/trending/movie/week",{
+      params:{
+          api_key: '205712c8b4bad38dc18a8f9c83c0f88e',
+          language: 'it-IT'
+      }
+  }
+  )
+  .then(function (response) {
+      // handle success
+      console.log(response);
+
+      resp.send(response.data)
+  })
+  .catch(function (error) {
+      // handle error
+      console.log(error);
+  })
+})
+
+app.get('/api/collection',  (req, resp) => {
+  const axios = require('axios').default;
+  axios.get("https://api.themoviedb.org/3/genre/movie/list",{
+    params:{
+      api_key: '205712c8b4bad38dc18a8f9c83c0f88e',
+      language: 'it-IT',
+    }
+  })
+  .then(function (response) {
+    // handle success
+    console.log(response);
+
+    resp.send(response.data)
+})
+.catch(function (error) {
+    // handle error
+    console.log(error);
+})
+})
+
+app.get('/api/ricercaPerGenere' , (req, resp) => {
+  const axios = require('axios').default;
+  const selectedPage = req.query.page;
+  const selectedGenre = req.query.genre
+  axios.get("https://api.themoviedb.org/3/discover/movie",{
+    params:{
+      api_key: '205712c8b4bad38dc18a8f9c83c0f88e',
+      language: 'it-IT',
+      sort_by: 'popularity.desc',
+      include_adult: false,
+      page: selectedPage,
+      with_genres: selectedGenre
+    }
+  })
+  .then(function (response) {
+    // handle success
+    console.log(response);
+
+    resp.send(response.data)
+})
+.catch(function (error) {
+    // handle error
+    console.log(error);
+})
 })
 
 app.listen(port, () => {
