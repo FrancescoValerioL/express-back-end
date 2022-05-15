106cf46e7555f7002d97bb947a6091315ea70f89
Francesco Valerio <70902884+FrancescoValerioL@users.noreply.github.com>
2022-05-15 15:38:02 +0200

    Sistemato e creato ricerca per genere TV e Film

--- index.js
+++ index.js
@@ -153,6 +153,58 @@ app.get("/api/tv/trendingWeek", (req, resp) => {
       resp.send(films);
     });
 });
+app.get('/api/tv/ricercaperGenere', (req, resp) => {
+  const axios = require('axios').default
+  const selectedPage = req.query.page;
+  const selectedGenre = req.query.genre.toLowerCase();
+  const img = 'https://image.tmdb.org/t/p/w500'
+  let series = []
+  let genres = new Map()
+  genres.set('azione', '10759')
+  genres.set('avventura', '10759')
+  genres.set('animazione', '16')
+  genres.set('commedia', '35')
+  genres.set('crime', '80')
+  genres.set('documentario', '99')
+  genres.set('dramma', '18')
+  genres.set('famiglia', '10751')
+  genres.set('kids', '10762')
+  genres.set('mistero', '9648')
+  genres.set('news', '10763')
+  genres.set('reality', '10764')
+  genres.set('fci-fi', '10765')
+  genres.set('fantasy', '10765')
+  genres.set('soap', '10766')
+  genres.set('talk', '10767')
+  genres.set('war', '10768')
+  genres.set('politics', '10768')
+  genres.set('western', '37')
+  axios
+    .get('https://api.themoviedb.org/3/discover/tv', {
+      params: {
+        api_key: "205712c8b4bad38dc18a8f9c83c0f88e",
+        language: "it-IT",
+        sort_by: "popularity.desc",
+        page: selectedPage,
+        with_genres: genres.get(selectedGenre),
+        include_null_first_air_dates: false,
+      }
+    })
+    .then(function(response) {
+      series = response.data.results.map((elem) => ({
+        title: elem.name,
+        id: elem.id,
+        img: img + elem.poster_path,
+        genre_ids: elem.genre_ids,
+        release_date: elem.release_date,
+      }))
+      resp.send(series)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+})
 
 // API MOVIE
 app.get("/api/movie", (req, resp) => {
@@ -321,7 +373,28 @@ app.get("/api/movie/collection", (req, resp) => {
 app.get("/api/movie/ricercaPerGenere", (req, resp) => {
   const axios = require("axios").default;
   const selectedPage = req.query.page;
-  const selectedGenre = req.query.genre;
+  const selectedGenre = req.query.genre.toLowerCase();
+  let genres = new Map()
+  genres.set('azione', '28')
+  genres.set('avventura', '12')
+  genres.set('animazione', '16')
+  genres.set('commedia', '35')
+  genres.set('crime', '80')
+  genres.set('documentario', '90')
+  genres.set('dramma', '18')
+  genres.set('famiglia', '10751')
+  genres.set('fantasy', '14')
+  genres.set('storia', '36')
+  genres.set('horror', '27')
+  genres.set('musica', '10402')
+  genres.set('mistero', '9648')
+  genres.set('romance', '10749')
+  genres.set('fantascienza', '878')
+  genres.set('sci-fi', '878')
+  genres.set('film', '10770')
+  genres.set('thriller', '53')
+  genres.set('guerra', '10752')
+  genres.set('western', '37')
   axios
     .get("https://api.themoviedb.org/3/discover/movie", {
       params: {
@@ -330,7 +403,7 @@ app.get("/api/movie/ricercaPerGenere", (req, resp) => {
         sort_by: "popularity.desc",
         include_adult: false,
         page: selectedPage,
-        with_genres: selectedGenre,
+        with_genres: genres.get(selectedGenre),
       },
     })
     .then(function (response) {
